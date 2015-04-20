199974791bba176f93123e12758ae76cdae665ab
Pavel Zbytovský <zbytovsky@gmail.com>
2015-04-20 18:42:06 +0200

    update tool: stažení nových změn

--- tools/update-gjk-repos.sh
+++ tools/update-gjk-repos.sh
@@ -5,6 +5,8 @@ for line in Kepler-GJK  meteo       MPplayer  space-challenge  treegen Lightbeat
 cd "$line";
 echo "$line";
 pwd
+git fetch origin
+git reset --hard origin/master
 git remote add gjkcz "git@github.com:gjkcz/$line.git"
 git push -f gjkcz master
 cd ..
